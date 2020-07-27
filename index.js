886e13dd5b24393d715dfa2b3dca51c8b7b8a865
Akshay <akshaypanchal201318@gmail.com>
2020-07-28 00:17:14 +0530

    connected the project to the database

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+
+const db = require('./config/mongoose');
+
 // importing the layout library
 
 const expressLayout = require('express-ejs-layouts');
